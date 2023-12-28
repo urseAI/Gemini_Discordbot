c03497c240192683f89b7b33473d42a4ee3ac575
urseAI <155104609+urseAI@users.noreply.github.com>
2023-12-28 11:16:35 -0800

    Update and rename .env.example to GeminiBotConfig.py

---
+++ GeminiBotConfig.py
@@ -0,0 +1,6 @@
+#Create at https://makersuite.google.com/
+GOOGLE_AI_KEY = "AIzaSyDPmSWC30vd3AjGqaG-04_5u2IwjnjSMyc"
+#Create at https://discord.com/developers/applications
+DISCORD_BOT_TOKEN = "MTE4NTg4NDE2MzI5NTk1NzA0Mg.GMV8Ap.yjhJ-g9UnjM-znWv_d8Nx7uBPQy3g6krdYsK8Y"
+#Disable history by setting to 0
+MAX_HISTORY = 15
